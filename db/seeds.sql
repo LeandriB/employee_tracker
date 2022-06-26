eb212fe6634751971991769bfab7b76ae33a40c0
Leandri Beavis <leandrikuyk@gmail.com>
2022-06-25 21:48:40 -0400

    added seeds to database

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO department (id, name)
+-- TODO: Add values
+VALUES 
+    ()
+
+
+INSERT INTO role (id, title, salary, department_id) 
+-- TODO: Add values
+VALUES
+    ()
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+-- TODO: Add values
+VALUES ();
