0f4520138aab0279f9779ddb010a34243736f21f
Leandri Beavis <leandrikuyk@gmail.com>
2022-06-26 22:27:14 -0400

    Receiving TypeError with current code

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+
 const { menu } = require('./js/queries');
 const { db } = require('./server')
 // const { departments, roles, employees, 
@@ -31,6 +32,88 @@ function mainMenu() {
             // TODO: update and delete employee    
             // TODO: add exit switch case for user
         }
+    })
+}
+
+function departments() {
+    let sql = 'SELECT * FROM department';
+    db.query(sql, function (err, results) {
+        if (err) 
+            throw err;
+        console.table(results);
+    });
+    mainMenu();
+}
+
+function roles() {
+    let sql = 'SELECT * FROM role';
+    db.query(sql, function (err, results) {
+        if (err) 
+            throw err;
+        console.table(results);
+    });
+    mainMenu();
+}
+
+function employees() {
+    let sql = 'SELECT * FROM employee';
+    db.query(sql, function (err, results) {
+        if (err) 
+            throw err;
+        console.table(results);
+    });
+    mainMenu();
+}
+
+function addDepartment() {
+    inquirer.prompt(departmentQueries)
+    .then(userInput => {
+        console.log(userInput)
+        db.query(`INSERT INTO department(name) VALUES 
+            ('${userInput.addDepartment}')`, 
+            function (err, results) {
+                if (err)
+                    throw err;
+                console.log(results);
+                console.log(`${userInput.addDep} successfully added`);
+        });
+        mainMenu();
+    })
+}
+
+function addRole() {
+    inquirer.prompt(roleQueries)
+    .then(userInput => {
+        console.log(userInput)
+        db.query(`INSERT INTO role(title, salary, department_id) VALUES 
+            ('${userInput.addRole}', 
+            ${userInput.addSalary},
+            ${userInput.addRoleID})`, 
+            function (err, results) {
+                if (err) 
+                    throw err;
+                console.log(results);
+                console.log(`${userInput.addRole} successfully added`);
+        });
+        mainMenu();
+    })
+}
+
+function addEmployee() {
+    inquirer.prompt(employeeQueries)
+    .then(userInput => {
+        console.log(userInput)
+        db.query(`INSERT INTO role(first_name, last_name, new_employee_role, new_employee_manager) VALUES 
+            ('${userInput.firstName}', 
+            ${userInput.lastName}',
+            ${userInput.employeeRole},
+            ${userInput.employeeManager})`,
+            function (err, results) {
+                if (err) 
+                    throw err;
+                console.log(results);
+                console.log(`${userInput.firstName} ${userInput.lastName} successfully added`);
+        })
     })
 }
 
